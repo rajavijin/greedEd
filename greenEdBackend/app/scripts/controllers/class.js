fb0ce84d386f24ba40ade9e69a70cc1f7ab48596
antojodickson <antojodickson@gmail.com>
2015-09-18 18:22:46 +0530

    greenEdBackend files

---
+++ greenEdBackend/app/scripts/controllers/class.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name greenEdApp.controller:ClassCtrl
+ * @description
+ * # ClassCtrl
+ * Controller of the greenEdApp
+ */
+angular.module('greenEdApp')
+  .controller('ClassCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ greenEdBackend/app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name greenEdApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the greenEdApp
+ */
+angular.module('greenEdApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
